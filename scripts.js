16654203a3555cd17561cb14a17ef3f13759f3e4
TJ Egan <twegan@us.ibm.com>
2017-02-21 21:01:08 -0600

    finish 19

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
+// const camera = document.querySelector('.takePhoto');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
@@ -15,6 +16,26 @@ function getVideo() {
     });
 }
 
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100;
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+  }
+
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0];
+    pixels.data[i + 100] = pixels.data[i + 1];
+    pixels.data[i - 150] = pixels.data[i + 2];
+  }
+
+  return pixels;
+}
+
 function paintCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
@@ -23,6 +44,13 @@ function paintCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    // Get pixels
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // Mess with pixels
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.1;
+    // Put them back
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -35,12 +63,13 @@ function takePhoto() {
   const data = canvas.toDataURL('image/jpeg');
   const link = document.createElement('a');
   link.href = data;
-  link.setAttribute('download', 'handsome');
-  link.innerHTML = `<img src="${data}" alt="Handsome" />`;
+  link.setAttribute('download', 'photobooth');
+  link.innerHTML = `<img src="${data}" alt="photobooth" />`;
   strip.insertBefore(link, strip.firstChild);
 }
 
 getVideo();
 
+canvas.addEventListener('click', takePhoto);
 video.addEventListener('canplay', paintCanvas);
 
